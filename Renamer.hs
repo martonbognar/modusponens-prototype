7bc0343d927ba512edcb1774f68dfd48c2a31137
marton bognar <martonbognar@sch.bme.hu>
2019-08-26 13:55:21 +0200

    [renamer] Finish monadic style refactor

--- Renamer.hs
+++ Renamer.hs
@@ -30,13 +30,12 @@ rnLookup v (SnocRnEnv env v' x)
   | Raw.eqRawVariable v v' = Just x
   | otherwise              = rnLookup v env
 
-
 rnFullExpr :: RnEnv -> Integer -> Raw.Expression -> (Expression, Integer)
 rnFullExpr env state0 exp = runState (rnExpr env exp) state0
 
-
-
 rnExpr :: RnEnv -> Raw.Expression -> RnM Expression
+rnExpr env (Raw.ExLit i) = return (ExLit i)
+rnExpr env Raw.ExTop     = return ExTop
 rnExpr env (Raw.ExVar x) = case rnLookup x env of
   Nothing -> error $ "Unbound variable " ++ show x -- fail miserably here
   Just y  -> return (ExVar y)
@@ -51,41 +50,19 @@ rnExpr env (Raw.ExApp e1 e2) = do
   e2' <- rnExpr env e2
   return (ExApp e1' e2')
 
-  -- ...add the rest of the cases here
+rnExpr env (Raw.ExMerge e1 e2) = do
+  e1' <- rnExpr env e1
+  e2' <- rnExpr env e2
+  return (ExMerge e1' e2')
 
+rnExpr env (Raw.ExAnn e t) = do
+  e' <- rnExpr env e
+  return (ExAnn e' (translateType t))
 
+rnExpr env (Raw.ExRec l e) = do
+  e' <- rnExpr env e
+  return (ExRec l e')
 
-
-renameH :: Raw.Expression -> Integer -> (Expression, Integer)
-renameH fullE s = case fullE of
-  (Raw.ExVar (Raw.MkRawVar v))   ->
-    (ExVar (MkVar x), s') where
-      (x, s') = runState new s
-  (Raw.ExLit i)                  ->
-    (ExLit i, s)
-  Raw.ExTop                      ->
-    (ExTop, s)
-  (Raw.ExAbs (Raw.MkRawVar v) e) ->
-    (ExAbs (MkVar x) e', s'') where
-      (x, s')   = runState new s
-      (e', s'') = renameH e s'
-  (Raw.ExApp e1 e2)              ->
-    (ExApp e1' e2', s') where
-      (e1', s1') = renameH e1 s
-      (e2', s')  = renameH e2 s1'
-  (Raw.ExMerge e1 e2)            ->
-    (ExMerge e1' e2', s') where
-      (e1', s1') = renameH e1 s
-      (e2', s')  = renameH e2 s1'
-  (Raw.ExAnn e t)                ->
-    (ExAnn e' (translateType t), s') where
-      (e', s') = renameH e s
-  (Raw.ExRec l e)                ->
-    (ExRec l e', s') where
-      (e', s') = renameH e s
-  (Raw.ExRecFld e l)             ->
-    (ExRecFld e' l, s') where
-      (e', s') = renameH e s
-
-rename :: Raw.Expression -> Expression
-rename ex = ex' where (ex', _) = renameH ex 0
+rnExpr env (Raw.ExRecFld e l) = do
+  e' <- rnExpr env e
+  return (ExRecFld e' l)
