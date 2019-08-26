8310751838f711a7095a8cc0ba4b57ae9c161df2
marton bognar <martonbognar@sch.bme.hu>
2019-08-26 11:32:39 +0200

    Start working on variable renaming

--- Renamer.hs
+++ Renamer.hs
@@ -1,8 +1,30 @@
 module Renamer where
 
+import Control.Monad
+
 import CommonTypes
 import qualified RawSyntax as Raw
 import Syntax
+
+newtype State s a = State { runState :: s -> (a, s) }
+
+instance Functor (State s) where
+  fmap transform (State stmt) = State $ \oldState ->
+    let (result, newState) = stmt oldState
+    in  (transform result, newState)
+
+instance Applicative (State s) where
+  pure  = return
+  (<*>) = ap
+
+instance Monad (State s) where
+  return x = State (\s -> (x, s))
+  (>>=) (State stmt) f = State $ \oldState ->
+          let (x1, midState) = stmt oldState in
+          runState (f x1) midState
+
+new :: State Integer Integer
+new = State (\s -> (s, s + 1))
 
 translateType :: Raw.Type -> Type
 translateType Raw.TyNat         = TyNat
@@ -11,13 +33,36 @@ translateType (Raw.TyArr t1 t2) = TyArr (translateType t1) (translateType t2)
 translateType (Raw.TyIs t1 t2)  = TyIs (translateType t1) (translateType t2)
 translateType (Raw.TyRec l t)   = TyRec l (translateType t)
 
+renameH :: Raw.Expression -> Integer -> (Expression, Integer)
+renameH fullE s = case fullE of
+  (Raw.ExVar (Raw.MkRawVar v))   ->
+    (ExVar (MkVar x), s') where
+      (x, s') = runState new s
+  (Raw.ExLit i)                  ->
+    (ExLit i, s)
+  Raw.ExTop                      ->
+    (ExTop, s)
+  (Raw.ExAbs (Raw.MkRawVar v) e) ->
+    (ExAbs (MkVar x) e', s'') where
+      (x, s')   = runState new s
+      (e', s'') = renameH e s'
+  (Raw.ExApp e1 e2)              ->
+    (ExApp e1' e2', s') where
+      (e1', s1') = renameH e1 s
+      (e2', s')  = renameH e2 s1'
+  (Raw.ExMerge e1 e2)            ->
+    (ExMerge e1' e2', s') where
+      (e1', s1') = renameH e1 s
+      (e2', s')  = renameH e2 s1'
+  (Raw.ExAnn e t)                ->
+    (ExAnn e' (translateType t), s') where
+      (e', s') = renameH e s
+  (Raw.ExRec l e)                ->
+    (ExRec l e', s') where
+      (e', s') = renameH e s
+  (Raw.ExRecFld e l)             ->
+    (ExRecFld e' l, s') where
+      (e', s') = renameH e s
+
 rename :: Raw.Expression -> Expression
-rename (Raw.ExVar (Raw.MkRawVar v))   = ExVar (MkVar 42)
-rename (Raw.ExLit i)                  = ExLit i
-rename Raw.ExTop                      = ExTop
-rename (Raw.ExAbs (Raw.MkRawVar v) e) = ExAbs (MkVar 42) (rename e)
-rename (Raw.ExApp e1 e2)              = ExApp (rename e1) (rename e2)
-rename (Raw.ExMerge e1 e2)            = ExMerge (rename e1) (rename e2)
-rename (Raw.ExAnn e t)                = ExAnn (rename e) (translateType t)
-rename (Raw.ExRec l e)                = ExRec l (rename e)
-rename (Raw.ExRecFld e l)             = ExRecFld (rename e) l
+rename ex = ex' where (ex', _) = renameH ex 0
